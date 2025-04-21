663727a184a6df6418f6509c473e58ed06c4794f
anonimus007-700 <sahsafg80@gmail.com>
2025-04-22 00:02:23 +0300

    dont know

--- lib.asm
+++ lib.asm
@@ -1,6 +1,14 @@
+section .data
+	fln dq "0.0001", 0
+	flm dq 1.1111
+	fmt db "%f", 10, 0
+
 section .text
 	global _start
 	global julia_quadratic
+
+	extern atof
+	extern printf
 
 _start:
 	xor rax, rax
@@ -10,55 +18,15 @@ julia_quadratic:		; zx, zy, cx, cy, threshold
 
 	mov rax, [rbp+24]
 	mov rbx, [rbp+32]
-	mov rcx, [rbp+38]
-	mov rdx, [rbp+46]
-	mov rsi, [rbp+54]
+	mov rcx, [rbp+40]
+	mov rdx, [rbp+48]
+	mov rsi, [rbp+56]
+
+	lea rdi, [rax]
+	call atof
 
 
-
-
-
-;	mov rsi, rax
-;	mov rax, 1
-;	mov rdi, 1
-;	mov rdx, 10
-;	syscall
-;
-;	mov rax, 60
-;	xor rdi, rdi
-;	syscall
-
-
-
-
-;	add rax, rbx
-;	add rcx, rdx
-;
-;	xor rdx, rdx
-;
-;.loop:	inc rdx
-;	cmp rdx, rsi
-;	je .finish
-;
-;	shl rax, 1		; faster multiplication
-;	mov rbx, rax
-;	sub rbx, rax
-;	add rax, rbx
-;
-;	add rax, rcx
-;
-;	and rax, 0x7FFFFFFF	; absolute value
-;
-;	cmp rax, 4
-;	jnge .loop		; if rax > 4
-;	mov rax, rdx
-;	jmp exit
-;
-;.finish:
-;	dec rsi
-;	mov rax, rsi
-;
-exit:	mov rsp, rbp
-	pop rbp
-	ret
+	mov rax, 60
+	xor rdi, rdi
+	syscall
 
